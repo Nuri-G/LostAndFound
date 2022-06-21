d3fe5e108a1cc862edf22bd40d42eef1f1d175c2
Nurivan Gomez <nurivan@fb.com>
2022-06-21 14:33:25 -0700

    Implemented Log In page.

---
+++ app/src/main/java/com/codepath/nurivan/lostandfound/LoginActivity.java
@@ -0,0 +1,55 @@
+package com.codepath.nurivan.lostandfound;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.codepath.nurivan.lostandfound.databinding.ActivityLoginBinding;
+import com.parse.ParseUser;
+
+public class LoginActivity extends AppCompatActivity {
+    public static final String TAG = "LoginActivity";
+    private ActivityLoginBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        binding.bLogIn.setOnClickListener(v -> {
+            String username = binding.etUsername.getText().toString();
+            String password = binding.etPassword.getText().toString();
+
+            loginUser(username, password);
+        });
+    }
+
+    private void loginUser(String username, String password) {
+
+        ParseUser.logOut();
+        ParseUser.logInInBackground(username, password, (user, e) -> {
+            if(e != null) {
+                Log.e(TAG, "Login issue", e);
+                Toast.makeText(this, "Failed to log in.", Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+
+            goMainActivity();
+
+            Toast.makeText(LoginActivity.this, "Logged in.", Toast.LENGTH_SHORT).show();
+        });
+    }
+
+    private void goMainActivity() {
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+        finish();
+    }
+}
--- app/src/main/java/com/codepath/nurivan/lostandfound/MainActivity.java
+++ app/src/main/java/com/codepath/nurivan/lostandfound/MainActivity.java
@@ -5,21 +5,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.parse.Parse;
 import com.parse.ParseObject;
+import com.parse.ParseUser;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = "MainActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ParseObject firstObject = new  ParseObject("FirstClass");
-        firstObject.put("message","Hey ! First message from android. Parse is now connected");
+        ParseObject firstObject = new ParseObject("FirstClass");
+        firstObject.put("message", "Hey ! First message from android. Parse is now connected");
         firstObject.saveInBackground(e -> {
-            if (e != null){
+            if (e != null) {
                 Log.e("MainActivity", e.getLocalizedMessage());
-            }else{
-                Log.d("MainActivity","Object saved.");
+            } else {
+                Log.d("MainActivity", "Object saved.");
             }
         });
+
+        Log.i(TAG, "Current User: " + ParseUser.getCurrentUser().getUsername());
     }
 }
