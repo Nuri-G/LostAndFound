0a17ea75a051d9954156bdb5c62b98503fa7af7c
Nurivan Gomez <nurivan@fb.com>
2022-06-23 19:45:56 -0700

    Added list of items on found items page.

---
+++ app/src/main/java/com/codepath/nurivan/lostandfound/adapters/ItemAdapter.java
@@ -0,0 +1,64 @@
+package com.codepath.nurivan.lostandfound.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.codepath.nurivan.lostandfound.R;
+import com.codepath.nurivan.lostandfound.databinding.ItemLayoutBinding;
+import com.codepath.nurivan.lostandfound.models.Item;
+
+import java.util.List;
+
+public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ViewHolder> {
+    Context context;
+    List<Item> items;
+
+    public ItemAdapter(Context context, List<Item> items) {
+        this.context = context;
+        this.items = items;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        ItemLayoutBinding binding = ItemLayoutBinding.inflate(LayoutInflater.from(context), parent, false);
+        return new ViewHolder(binding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Item item = items.get(position);
+        holder.bind(item);
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        ItemLayoutBinding binding;
+        public ViewHolder(ItemLayoutBinding binding) {
+            super(binding.getRoot());
+
+            this.binding = binding;
+        }
+
+        public void bind(Item item) {
+            binding.tvItemName.setText(item.getItemName());
+
+            double latitude = item.getItemLocation().getLatitude();
+            double longitude = item.getItemLocation().getLongitude();
+
+            String coordinates = "(" + latitude + ", " + longitude + ")";
+
+            binding.tvCoordinates.setText(coordinates);
+            binding.tvDate.setText(item.getCreatedAt().toString());
+        }
+    }
+}
--- app/src/main/java/com/codepath/nurivan/lostandfound/fragments/FoundFragment.java
+++ app/src/main/java/com/codepath/nurivan/lostandfound/fragments/FoundFragment.java
@@ -5,19 +5,32 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.codepath.nurivan.lostandfound.activities.ItemNameActivity;
+import com.codepath.nurivan.lostandfound.adapters.ItemAdapter;
 import com.codepath.nurivan.lostandfound.databinding.FragmentFoundBinding;
 import com.codepath.nurivan.lostandfound.models.FoundItem;
 import com.codepath.nurivan.lostandfound.models.Item;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class FoundFragment extends Fragment {
+    public static final String TAG = "FoundFragment";
 
     private FragmentFoundBinding binding;
+    private ItemAdapter adapter;
+    private List<Item> items;
 
     public FoundFragment() {
         // Required empty public constructor
@@ -26,6 +39,7 @@ public class FoundFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        items = new ArrayList<>();
     }
 
     @Override
@@ -38,6 +52,12 @@ public class FoundFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         binding.bAdd.setOnClickListener(v -> showItemNameActivity());
+        binding.rvFoundItems.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        adapter = new ItemAdapter(getContext(), items);
+        binding.rvFoundItems.setAdapter(adapter);
+
+        getFoundItems();
     }
 
     @Override
@@ -52,5 +72,22 @@ public class FoundFragment extends Fragment {
         i.putExtra(Item.class.getSimpleName(), item);
 
         startActivity(i);
+    }
+
+    private void getFoundItems() {
+        items.clear();
+        ParseQuery<FoundItem> query = ParseQuery.getQuery(FoundItem.class);
+        query.whereEqualTo(FoundItem.KEY_FOUND_BY, ParseUser.getCurrentUser());
+        query.setLimit(20);
+        binding.pbFoundItems.setVisibility(View.VISIBLE);
+        query.findInBackground((objects, e) -> {
+            if(e != null) {
+                Log.e(TAG, "Error getting found items", e);
+                return;
+            }
+            items.addAll(objects);
+            adapter.notifyDataSetChanged();
+            binding.pbFoundItems.setVisibility(View.GONE);
+        });
     }
 }
