03505ef9f8ba163dd73ab35ddecadbe64171f2d6
Nurivan Gomez <nurivan@fb.com>
2022-06-29 15:55:04 -0700

    Removed local match calculation.

--- app/src/main/java/com/codepath/nurivan/lostandfound/models/Item.java
+++ app/src/main/java/com/codepath/nurivan/lostandfound/models/Item.java
@@ -3,10 +3,8 @@ package com.codepath.nurivan.lostandfound.models;
 import com.parse.ParseGeoPoint;
 import com.parse.ParseObject;
 
-import org.apache.commons.text.similarity.LevenshteinDistance;
 import org.json.JSONArray;
 
-import java.time.temporal.ChronoUnit;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -41,67 +39,6 @@ public abstract class Item extends ParseObject {
 
     public Item getConfirmedMatch() {
         return (Item) getParseObject(KEY_CONFIRMED_MATCH);
-    }
-
-    //Returns value from 0 to 1 depending on how close to 0 the distance is.
-    private double locationSimilarity(Item other) {
-        final double MAX_DISTANCE = 50.0;
-        double distanceMiles = getItemLocation().distanceInMilesTo(other.getItemLocation());
-
-        if(distanceMiles > MAX_DISTANCE) {
-            return 0;
-        } else {
-            return (MAX_DISTANCE - distanceMiles) / MAX_DISTANCE;
-        }
-    }
-
-    private double nameSimilarity(Item other) {
-        String itemName = getItemName().toLowerCase();
-        String otherName = other.getItemName().toLowerCase();
-        int longerLength = Math.max(itemName.length(), otherName.length());
-        LevenshteinDistance nameSimilarity = new LevenshteinDistance();
-
-        int similarity = nameSimilarity.apply(itemName, otherName);
-
-        return (longerLength - similarity) / (double) longerLength;
-    }
-
-    private double timeSimilarity(Item other) {
-        LostItem lostItem;
-        FoundItem foundItem;
-        if(this instanceof LostItem) {
-            lostItem = (LostItem) this;
-            foundItem = (FoundItem) other;
-        } else {
-            lostItem = (LostItem) other;
-            foundItem = (FoundItem) this;
-        }
-
-        Date lostDate = lostItem.getTimeLost();
-        Date foundDate = foundItem.getTimeFound();
-
-        int daysBetween = (int) ChronoUnit.DAYS.between(lostDate.toInstant(), foundDate.toInstant());
-
-        final int MAX_DAYS_AFTER = 14;
-        final int MAX_DAYS_BEFORE = 1;
-
-        if(daysBetween > -MAX_DAYS_BEFORE && daysBetween < MAX_DAYS_AFTER) {
-            return (MAX_DAYS_AFTER - daysBetween) / (double) MAX_DAYS_AFTER;
-        }
-        return 0;
-    }
-
-    public double checkItemMatch(Item other) {
-        final double NAME_SIMILARITY_WEIGHT = 0.4;
-        final double LOCATION_SIMILARITY_WEIGHT = 0.3;
-        final double TIME_SIMILARITY_WEIGHT = 0.3;
-
-        double nameSimilarity = nameSimilarity(other);
-        double locationSimilarity = locationSimilarity(other);
-        double timeSimilarity = timeSimilarity(other);
-
-
-        return NAME_SIMILARITY_WEIGHT * nameSimilarity + LOCATION_SIMILARITY_WEIGHT * locationSimilarity + TIME_SIMILARITY_WEIGHT * timeSimilarity;
     }
 
     public static String formatItemDate(Date date) {
