36a64f24b1afe468effe0fa29608efab216940d2
Nurivan Gomez <nurivan@fb.com>
2022-07-07 16:43:20 -0700

    Clicking on map pin description opens item details.

--- app/src/main/java/com/codepath/nurivan/lostandfound/activities/MainActivity.java
+++ app/src/main/java/com/codepath/nurivan/lostandfound/activities/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -18,60 +19,68 @@ import com.codepath.nurivan.lostandfound.models.FoundItem;
 import com.codepath.nurivan.lostandfound.models.Item;
 import com.codepath.nurivan.lostandfound.models.LostItem;
 import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.parse.ParseGeoPoint;
 import com.parse.ParseUser;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
     public static final String TAG = "MainActivity";
 
-    private ActivityMainBinding binding;
     private FragmentManager fragmentManager;
 
     private final Fragment lostFragment = new LostFragment();
     private final Fragment foundFragment = new FoundFragment();
     private final Fragment profileFragment = new ProfileFragment();
     private final Fragment mapFragment = SupportMapFragment.newInstance();
+    private final HashMap<Marker, Item> markerItems = new HashMap<>();
+
+    private Fragment currentFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        com.codepath.nurivan.lostandfound.databinding.ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
 
         fragmentManager = getSupportFragmentManager();
 
         binding.bottomNavigation.setOnItemSelectedListener(item -> {
-            Fragment fragment;
+
 
             int itemId = item.getItemId();
 
             if(itemId == R.id.action_lost) {
-                fragment = lostFragment;
+                currentFragment = lostFragment;
             } else if(itemId == R.id.action_found) {
-                fragment = foundFragment;
+                currentFragment = foundFragment;
             } else if(itemId == R.id.action_profile) {
-                fragment = profileFragment;
+                currentFragment = profileFragment;
             } else if(itemId == R.id.action_map) {
-                fragment = mapFragment;
-                setMapPins();
+                currentFragment = mapFragment;
+                ((SupportMapFragment) mapFragment).getMapAsync(this);
             } else {
                 return true;
             }
 
-            fragmentManager.beginTransaction().replace(R.id.fragment_holder, fragment).commit();
+            fragmentManager.beginTransaction().replace(R.id.fragment_holder, currentFragment).commit();
 
             return true;
         });
 
-        binding.bottomNavigation.setSelectedItemId(R.id.action_lost);
+        if(currentFragment == null) {
+            binding.bottomNavigation.setSelectedItemId(R.id.action_lost);
+        }
 
         Log.i(TAG, "Current User: " + ParseUser.getCurrentUser().getUsername());
     }
@@ -99,32 +108,30 @@ public class MainActivity extends AppCompatActivity {
         return new LatLng(latitude, longitude);
     }
 
-    private void setMapPins() {
-        ((SupportMapFragment) mapFragment).getMapAsync(googleMap -> {
-            googleMap.clear();
-            List<Item> allItems = new ArrayList<>();
-            allItems.addAll(LostFragment.getItemList());
-            allItems.addAll(FoundFragment.getItemList());
-            for(Item item : allItems) {
-                MarkerOptions options = new MarkerOptions();
-                options.title(item.getItemName());
-                options.snippet(Item.formatItemCoordinates(item.getItemLocation()));
-                ParseGeoPoint location = item.getItemLocation();
-                LatLng point = new LatLng(location.getLatitude(), location.getLongitude());
-                options.position(point);
-                if(item instanceof LostItem) {
-                    options.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA));
-                } else if(item instanceof FoundItem) {
-                    options.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE));
-                }
-
-
-                googleMap.addMarker(options);
+    private void setMapPins(GoogleMap googleMap) {
+        googleMap.clear();
+        List<Item> allItems = new ArrayList<>();
+        allItems.addAll(((LostFragment) lostFragment).getItemList());
+        allItems.addAll(((FoundFragment) foundFragment).getItemList());
+        for(Item item : allItems) {
+            MarkerOptions options = new MarkerOptions();
+            options.title(item.getItemName());
+            options.snippet(Item.formatItemCoordinates(item.getItemLocation()));
+            ParseGeoPoint location = item.getItemLocation();
+            LatLng point = new LatLng(location.getLatitude(), location.getLongitude());
+            options.position(point);
+            if(item instanceof LostItem) {
+                options.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA));
+            } else if(item instanceof FoundItem) {
+                options.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE));
             }
 
-            LatLng avgLocation = calculateAverageLocation(allItems);
-            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(avgLocation, 2));
-        });
+            Marker m = googleMap.addMarker(options);
+            markerItems.put(m, item);
+        }
+
+        LatLng avgLocation = calculateAverageLocation(allItems);
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(avgLocation, 2));
     }
 
     @Override
@@ -135,11 +142,19 @@ public class MainActivity extends AppCompatActivity {
             Item item = intent.getParcelableExtra(Item.class.getSimpleName());
             if(item instanceof LostItem) {
                 ((LostFragment) lostFragment).addLostItem((LostItem) item);
-                binding.bottomNavigation.setSelectedItemId(R.id.action_lost);
             } else if(item instanceof FoundItem) {
                 ((FoundFragment) foundFragment).addFoundItem((FoundItem) item);
-                binding.bottomNavigation.setSelectedItemId(R.id.action_found);
             }
         }
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        setMapPins(googleMap);
+        googleMap.setOnInfoWindowClickListener(marker -> {
+            Intent i = new Intent(MainActivity.this, ItemDetailsActivity.class);
+            i.putExtra(Item.class.getSimpleName(), markerItems.get(marker));
+            startActivity(i);
+        });
     }
 }
