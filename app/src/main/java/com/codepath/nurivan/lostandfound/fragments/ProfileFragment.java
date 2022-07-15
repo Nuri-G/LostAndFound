48d2ac8718dd5be8cbf803afa81cd37bf6580f08
Nurivan Gomez <nurivan@fb.com>
2022-07-15 10:07:43 -0700

    Updated user profile layout to include more information.

--- app/src/main/java/com/codepath/nurivan/lostandfound/fragments/ProfileFragment.java
+++ app/src/main/java/com/codepath/nurivan/lostandfound/fragments/ProfileFragment.java
@@ -3,14 +3,18 @@ package com.codepath.nurivan.lostandfound.fragments;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
 import com.codepath.nurivan.lostandfound.activities.LogInActivity;
+import com.codepath.nurivan.lostandfound.activities.SignUpActivity;
 import com.codepath.nurivan.lostandfound.databinding.FragmentProfileBinding;
+import com.parse.GetCallback;
 import com.parse.ParseUser;
 
 
@@ -34,10 +38,59 @@ public class ProfileFragment extends Fragment {
         return binding.getRoot();
     }
 
+    private String formatEmail(String email) {
+        if(email.length() > 18) {
+            email = email.substring(0, 15);
+            email += "...";
+        }
+        return email;
+    }
+
+    private String formatHomeAddress(String homeAddress) {
+        if(homeAddress == null) {
+            homeAddress = "";
+        }
+        String out = homeAddress;
+        int comma = homeAddress.indexOf(',');
+
+        if(comma >= 0 && comma < homeAddress.length() - 1) {
+            out = homeAddress.substring(0, comma + 1);
+            out += '\n';
+            out += homeAddress.substring(comma + 1);
+        }
+        return out;
+    }
+
     @Override
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
-        binding.tvUsername.setText(ParseUser.getCurrentUser().getUsername());
+        ParseUser user = ParseUser.getCurrentUser();
+        binding.tvUsername.setText(user.getUsername());
+        binding.tvEmail.setText(formatEmail(user.getEmail()));
+        binding.tvHomeAddress.setText(formatHomeAddress((String) user.get("homeAddress")));
         binding.bLogOut.setOnClickListener(v -> logOutUser());
+        binding.bEditProfile.setOnClickListener(v -> showSignUpActivity());
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        ParseUser.getCurrentUser().fetchInBackground((GetCallback<ParseUser>) (user, e) -> {
+            if(e != null) {
+                Toast.makeText(getContext(), "Failed to fetch user details.", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            if(binding != null) {
+                binding.tvUsername.setText(user.getUsername());
+                binding.tvEmail.setText(formatEmail(user.getEmail()));
+                binding.tvHomeAddress.setText(formatHomeAddress((String) user.get("homeAddress")));
+            }
+        });
+    }
+
+    private void showSignUpActivity() {
+        Intent i = new Intent(getContext(), SignUpActivity.class);
+        startActivity(i);
     }
 
     @Override
